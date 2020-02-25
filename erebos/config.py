e06200944fb708288c4a229bb907c3406b26e274
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-02-25 07:27:26 -0700

    add sentry integration

--- erebos/config.py
+++ erebos/config.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
 
+import sentry_dramatiq
+import sentry_sdk
 from starlette.config import Config
 
 
 config = Config(".env")
 
+SENTRY_DSN = config("SENTRY_DSN", default=None)
 PROXY = config("PROXY", default="/erebos")
 LOG_LEVEL = config("LOG_LEVEL", default="INFO")
 ZARR_DIR = config("ZARR_DIR", cast=Path, default="/d4/uaren/goes/G16/erebos/zarr/")
@@ -15,4 +18,8 @@ MULTI_DIR = config(
 )
 REDIS_HOST = config("REDIS_HOST", default="localhost")
 REDIS_PORT = config("REDIS_PORT", default=6379)
-MEM_LIMIT = config('RSS_MEMORY_LIMIT', default=384.0, cast=float)
+MEM_LIMIT = config("RSS_MEMORY_LIMIT", default=384.0, cast=float)
+
+
+if SENTRY_DSN is not None:
+    sentry_sdk.init(SENTRY_DSN, integrations=[sentry_dramatiq.DramatiqIntegration])
